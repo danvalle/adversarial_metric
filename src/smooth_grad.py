472678eca2aebbe559bf799f71904ebc2ed5dc39
Dan Valle <danvalle05@gmail.com>
2018-12-13 23:53:51 -0200

    smgrad update

--- src/smooth_grad.py
+++ src/smooth_grad.py
@@ -38,35 +38,44 @@ def main(num_images, device_name, seed):
     torch.manual_seed(seed)
     device = torch.device(device_name)
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((224,224)),
         transforms.ToTensor()])
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225])
 
     data = load_data()
     model = models.vgg16(pretrained=True).to(device=device)
     model.eval()
     criterion = nn.CrossEntropyLoss()
 
+    sotfmax = nn.Softmax(dim=1)
+    print('Name,Label,Pred,Conf,Loss')
     it = 0
     for name, (img, label) in data.items():
-        image = Image.open(img)
-        X = transform(image)
+        image = transform(Image.open(img).convert('RGB')).to(device)
+        X = normalize(image.clone())
         save_image(X, 'img/original/'+name+'.png')
 
         X = X.to(device=device)
         X.requires_grad_()        
         out = model(X.unsqueeze(0))
-        print(name, ':', out.argmax().item(), label)
         
         loss = criterion(out, out.argmax().view(-1))
         loss.backward()
 
+        # Save log
+        print('{},{},{},{},{}'.format(
+            name, label, out.argmax().item(),
+            sotfmax(out.detach().clone()).max(), loss.item()))
+
         simple_grad = X.grad.data.clone()
         with torch.no_grad():
-            visualization(simple_grad, 'img/simple_grad/'+name+'.png', X)
+            visualization(simple_grad, 'img/simple_grad/', name+'.png', image)
 
         sm_grad = smooth_grad(X, 100, 0.2, model, criterion, device)
         with torch.no_grad():
-            visualization(sm_grad, 'img/smooth_grad/'+name+'.png', X)
+            visualization(sm_grad, 'img/smooth_grad/', name+'.png', image)
 
         it += 1
         if it == num_images:
@@ -74,7 +83,7 @@ def main(num_images, device_name, seed):
 
 
 if __name__ == '__main__':
-    num_images = 10
+    num_images = 15
     device_name = 'cuda'
     seed = 0
 
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import os
 import pickle as pkl
 
 import numpy as np
@@ -10,11 +11,40 @@ def load_data():
     return data
 
 
-def visualization(tensor, file_name, image):
+def normalize(tensor):
+    min_value = tensor.min()
+    rg = tensor.max() - min_value
+    print(tensor.min(), tensor.max())
+    return (tensor - min_value) / rg
+
+
+def visualization(tensor, file_path, file_name, image):
     tensor.abs_()
     
     percentile = np.percentile(tensor, 99)
     tensor.clamp_(0, percentile)
 
+    # Pure relevnce in 3 channels
+    complete_path = file_path + 'clamp/'
+    if not os.path.exists(complete_path):
+        os.makedirs(complete_path)
+    save_image(tensor*1000, complete_path+file_name, normalize=True)
+
+    # Relevance map
+    complete_path = file_path + 'map/'
+    if not os.path.exists(complete_path):
+        os.makedirs(complete_path)
+    save_image(tensor.sum(dim=0)*1000, complete_path+file_name, normalize=True)
+
+    # Relevance x image / 3 channels and map
     tensor.mul_(image)
-    save_image(tensor.sum(dim=0), file_name, normalize=True)
+
+    complete_path = file_path + 'final/'
+    if not os.path.exists(complete_path):
+        os.makedirs(complete_path)
+    save_image(tensor*1000, complete_path+file_name, normalize=True)
+
+    complete_path = file_path + 'final_map/'
+    if not os.path.exists(complete_path):
+        os.makedirs(complete_path)
+    save_image(tensor.sum(dim=0)*1000, complete_path+file_name, normalize=True)
